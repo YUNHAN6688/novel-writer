fba82df34054614cc00a85649e2316621a56daef
YUNHAN6688 <2510409008@qq.com>
2026-08-27 20:15:56 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from ui_builder import build_main_window
-from data_handler import load_novel_data
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    novel_data = load_novel_data()
-    build_main_window(root, novel_data)
-    root.mainloop()
